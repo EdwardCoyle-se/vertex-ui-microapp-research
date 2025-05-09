5ec556d14923e344b9af001afab543ea0c7f3ea8
Ed Coyle <ed.coyle@se.com>
2025-05-09 13:14:43 -0400

    Add microapp connections and basic routing

--- projects/container/src/app/app.routes.ts
+++ projects/container/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './routes/home/home.component';
+import { loadRemoteModule } from '@angular-architects/native-federation';
+import { NotFoundComponent } from './routes/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'microapp-a',
+    loadComponent: () => loadRemoteModule('microapp-a', './Component').then((m) => m.AppComponent)
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
